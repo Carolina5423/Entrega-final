d7eee26e81190fff6002c30983b1fbb9f87d5989
Carolina5423 <carolina.olmoss@gmail.com>
2023-12-07 22:49:52 -0300

    segunda parte

--- src/componentes/ItemCount.jsx
+++ src/componentes/ItemCount.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const ItemCount = ( {cantidad, handleRestar, handleSumar, handleAgregar} ) => {
+const ItemCount = ({ cantidad, handleRestar, handleSumar, handleAgregar }) => {
 
-  return (
-    <div>
+    return (
+        <div>
 
-        <div className="item-count">
-            <button onClick={handleRestar}>-</button>
-            <p>{cantidad}</p>
-            <button onClick={handleSumar}>+</button>
+            <div className="item-count">
+                <button onClick={handleRestar}>-</button>
+                <p>{cantidad}</p>
+                <button onClick={handleSumar}>+</button>
+            </div>
+            <button className="agregar-al-carrito" onClick={handleAgregar}>Agregar al carrito</button>
         </div>
-        <button className="agregar-al-carrito" onClick={handleAgregar}>Agregar al carrito</button>
-    </div>
-  )
+    )
 }
 
 export default ItemCount
--- src/componentes/ItemDetail.jsx
+++ src/componentes/ItemDetail.jsx
@@ -4,7 +4,7 @@ import ItemCount from "./ItemCount"
 import { CartContext } from "../context/CartContext";
 
 
-const ItemDetail = ( {item} ) => {
+const ItemDetail = ({ item }) => {
 
     const { carrito, agregarAlCarrito } = useContext(CartContext);
     console.log(carrito);
@@ -19,25 +19,25 @@ const ItemDetail = ( {item} ) => {
         cantidad < item.stock && setCantidad(cantidad + 1)
     }
 
-  return (
-    <div className="container">
-        <div className="producto-detalle">
-            <img src={item.imagen} alt={item.titulo} />
-            <div>
-                <h3 className="titulo">{item.titulo}</h3>
-                <p className="descripcion">{item.descripcion}</p>
-                <p className="categoria">Categoría: {toCapital(item.categoria)}</p>
-                <p className="precio">${item.precio}</p>
-                <ItemCount
-                  cantidad={cantidad}
-                  handleSumar={handleSumar}
-                  handleRestar={handleRestar}
-                  handleAgregar={() => { agregarAlCarrito(item, cantidad) }}
-                />
+    return (
+        <div className="container">
+            <div className="producto-detalle">
+                <img src={item.imagen} alt={item.titulo} />
+                <div>
+                    <h3 className="titulo">{item.titulo}</h3>
+                    <p className="descripcion">{item.descripcion}</p>
+                    <p className="categoria">Categoría: {toCapital(item.categoria)}</p>
+                    <p className="precio">${item.precio}</p>
+                    <ItemCount
+                        cantidad={cantidad}
+                        handleSumar={handleSumar}
+                        handleRestar={handleRestar}
+                        handleAgregar={() => { agregarAlCarrito(item, cantidad) }}
+                    />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ItemDetail
