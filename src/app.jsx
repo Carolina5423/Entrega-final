89afc62e588e350ba21796d56512baf96d83c9aa
Carolina5423 <carolina.olmoss@gmail.com>
2023-12-20 11:29:02 -0300

    jsx

--- src/app.jsx
+++ src/app.jsx
@@ -10,28 +10,28 @@ import Carrito from "./components/Carrito";
 import Checkout from "./components/Checkout";
 
 function App() {
-  return (
-    <div>
-      <CartProvider>
-        <BrowserRouter>
+    return (
+        <div>
+            <CartProvider>
+                <BrowserRouter>
 
-          <Navbar />
+                    <Navbar />
 
-          <Routes>
-            <Route path="/" element={<ItemListContainer />} />
-            <Route path="/item/:id" element={<ItemDetailContainer />}/>
-            <Route path="/productos" element={<ItemListContainer />} />
-            <Route path="/productos/:categoria" element={<ItemListContainer />} />
-            <Route path="/nosotros" element={<Nosotros />}/>
-            <Route path="/contacto" element={<Contacto />}/>
-            <Route path="/carrito" element={<Carrito />}/>
-            <Route path="/checkout" element={<Checkout />}/>
-          </Routes>
-          
-        </BrowserRouter>
-      </CartProvider>
-    </div>
-  );
+                    <Routes>
+                        <Route path="/" element={<ItemListContainer />} />
+                        <Route path="/item/:id" element={<ItemDetailContainer />} />
+                        <Route path="/productos" element={<ItemListContainer />} />
+                        <Route path="/productos/:categoria" element={<ItemListContainer />} />
+                        <Route path="/nosotros" element={<Nosotros />} />
+                        <Route path="/contacto" element={<Contacto />} />
+                        <Route path="/carrito" element={<Carrito />} />
+                        <Route path="/checkout" element={<Checkout />} />
+                    </Routes>
+
+                </BrowserRouter>
+            </CartProvider>
+        </div>
+    );
 }
 
 export default App;
